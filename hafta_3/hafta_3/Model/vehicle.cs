f9147ea9fc9e2f1969bc5c924eaa14c75f19fbb0
Enes Ağcahan <98733474+EnesAgcahan@users.noreply.github.com>
2022-08-29 00:27:22 +0300

    Add files via upload

---
+++ hafta_3/hafta_3/Model/vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace EnesAgcahan_Hafta3.Model
+{
+    public class vehicle
+    {
+        public virtual long id { get; set; }
+        public virtual string? vehiclename { get; set; }
+        public virtual string? vehicleplate { get; set; }
+
+    }
+}
