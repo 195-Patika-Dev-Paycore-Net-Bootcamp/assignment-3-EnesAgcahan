24eaf0c471192936594ed942464ed660c65edcba
Enes Ağcahan <98733474+EnesAgcahan@users.noreply.github.com>
2022-09-09 14:27:53 +0300

    Add files via upload

---
+++ hafta_3/hafta_3/Controllers/ListIdController.cs
@@ -0,0 +1,33 @@
+﻿using EnesAgcahan_Hafta3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace EnesAgcahan_Hafta3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListIdController : ControllerBase
+    {
+
+        private readonly IMapperSession session;
+        public ListIdController(IMapperSession session)
+        {
+            this.session = session;
+        }
+        [HttpGet("{Cluster}")]
+
+
+        public ActionResult<List<List<container>>> GetContainerPiecesByVehicleId(int vehicleId, int pieces)
+        {
+
+            List<container> result = session.Containers.Where(x => x.vehicleid == vehicleId).ToList();
+            List<List<container>> division = new List<List<container>>(pieces);
+            division = result.Select((x, i) => new { Index = i, Value = x }).GroupBy(x => x.Index % pieces).Select(x => x.Select(c => c.Value).ToList()).ToList();
+
+            return Ok(division);
+        }
+    }
+}
---
+++ hafta_3/hafta_3/Controllers/VehicleController.cs
@@ -0,0 +1,129 @@
+﻿using EnesAgcahan_Hafta3.Context;
+using EnesAgcahan_Hafta3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnesAgcahan_Hafta3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+
+        private readonly IMapperSession session;
+        public VehicleController(IMapperSession session)
+        {
+            this.session = session;
+        }
+        //Araç listesi
+        [HttpGet]
+        public List<vehicle> Get()
+        {
+            List<vehicle> result = session.Vehicles.ToList();
+            return result;
+        }
+
+        
+        //id bazlı araç get işlemi
+        [HttpGet("{id}")]
+        public vehicle Get(long id)
+        {
+            vehicle result = session.Vehicles.Where(x => x.id == id).FirstOrDefault();
+            return result;
+        }
+        //Araç güncelleme işlemi
+        [HttpPost]
+        public void Post([FromBody] vehicle vehicle)
+        {
+            try
+            {
+                session.BeginTransaction();
+                session.Save(vehicle);
+                session.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+        }
+
+        //Araç güncelleme işlemi
+        [HttpPut]
+        public ActionResult<vehicle> Put([FromBody] vehicle request)
+        {
+            vehicle vehicle = session.Vehicles.Where(x => x.id == request.id).FirstOrDefault();
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                session.BeginTransaction();
+
+                
+                vehicle.id = request.id;
+                vehicle.vehiclename = request.vehiclename;
+                vehicle.vehicleplate = request.vehicleplate;
+
+
+
+                session.Update(vehicle);
+
+                session.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+
+
+            return Ok();
+        }
+
+        //Araç silme işlemi
+        [HttpDelete("{id}")]
+        public ActionResult<vehicle> Delete(long id)
+        {
+            vehicle vehicle = session.Vehicles.Where(x => x.id == id).FirstOrDefault();
+            
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                session.BeginTransaction();
+                session.Delete(vehicle);
+                session.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+
+            return Ok();
+        }
+
+
+    }
+}
